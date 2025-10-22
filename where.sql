f15a92368839936b6e024190bfc0eb47a72236c9
Victor Lovetro <victor.neves.lovetro11@gmail.com>
2025-10-22 19:19:30 -0300

    Update where.sql
    Added SQL queries to filter payments and addresses.

--- where.sql
+++ where.sql
@@ -2,3 +2,9 @@ use sakila;
 select * from payment
 --order by amount  desc
 where amount != 0.99
+
+--Pode ser usado para encontrar informações no banco de dado 
+use sakila;
+Select * from address
+
+where district != 'Texas'
