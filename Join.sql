77d25d09f90cebb0334cf67f98636dd405e4b136
Victor Lovetro <victor.neves.lovetro11@gmail.com>
2025-10-23 20:00:51 -0300

    Create Join.sql

---
+++ Join.sql
@@ -0,0 +1,11 @@
+use sakila;
+
+select 
+    customer.customer_id, 
+    customer.first_name,
+    customer.last_name,
+    payment.rental_id,
+    payment.amount
+from customer 
+JOIN payment  ON customer.customer_id = payment.payment_id
+#Juntando duas tabelas do banco de dados
