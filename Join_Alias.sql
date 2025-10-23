ebca37c81528c0e96123ef726cc276d2eac39dbb
Victor Lovetro <victor.neves.lovetro11@gmail.com>
2025-10-23 20:12:12 -0300

    Update Join_Alias.sql

--- Join_Alias.sql
+++ Join_Alias.sql
@@ -21,3 +21,20 @@ select
 from customer cus # temos que citar o Alias a frente da variável
 JOIN payment pay ON cus.customer_id = pay.payment_id
 #Juntando duas tabelas do banco de dados
+
+    #Puxando vário JOIN
+use sakila;
+
+select 
+    cus.customer_id, 
+    cus.first_name,
+    cus.last_name,
+    adr.address,
+    pay.rental_id,
+    pay.amount
+from customer cus # temos que citar o Alias a frente da variável
+JOIN payment pay 
+    ON cus.customer_id = pay.payment_id
+#Juntando duas tabelas do banco de dados
+JOIN address adr 
+    ON cus.customer_id = adr.address_id
