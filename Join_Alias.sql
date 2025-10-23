ced5ae5879ab70e1a05c2f459e37ab9e703eea8d
Victor Lovetro <victor.neves.lovetro11@gmail.com>
2025-10-23 20:06:35 -0300

    Update and rename Join.sql to Join_Alias.sql
    Updated SQL query to use aliases for better readability.

---
+++ Join_Alias.sql
@@ -0,0 +1,23 @@
+use sakila;
+
+select 
+    customer.customer_id, 
+    customer.first_name,
+    customer.last_name,
+    payment.rental_id,
+    payment.amount
+from customer 
+JOIN payment  ON customer.customer_id = payment.payment_id
+#Juntando duas tabelas do banco de dados
+
+use sakila;
+
+select 
+    cus.customer_id, 
+    cus.first_name,
+    cus.last_name,
+    pay.rental_id,
+    pay.amount
+from customer cus # temos que citar o Alias a frente da variável
+JOIN payment pay ON cus.customer_id = pay.payment_id
+#Juntando duas tabelas do banco de dados
